7928b8b16dcbbbd8cb263720f269f388d3f75099
Henry.Lima <124622326+Henry-Lima@users.noreply.github.com>
2024-11-07 22:29:31 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Projeto-Final
+# Consumo-de-API
+fazendo consumo de API utilizando rotas com tabela em sql e node, em um front simples com html e css 
---
+++ index.js
@@ -0,0 +1,99 @@
+const Sequelize = require("sequelize");
+const express = require("express");
+const cors = require("cors");
+
+const app = express(); 
+const { create } = require("express-handlebars");
+app.use(cors());
+
+
+const conexaoComBanco = new Sequelize("gerenciamento", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+
+app.get("/aps/:Nome/:Descricao/:Data/:Funcionarios", function (req, res) {
+    res.send({
+        nome: req.params.Nome,
+        descricao: req.params.Descricao,
+        data: req.params.Data,
+        funcionarios: req.params.Funcionarios,
+    }); 
+});
+
+
+const Projeto = conexaoComBanco.define("projetos", {
+  nome: {
+      type: Sequelize.STRING,
+  },
+
+  descricao: {
+      type: Sequelize.STRING,
+  },
+
+  data: {
+      type: Sequelize.DATE,
+  },
+
+  funcionarios: {
+      type: Sequelize.STRING,
+  },
+});
+
+
+Projeto.sync({ force: false }); 
+
+app.get("/mostrar", async function (req, res) {
+
+  try {
+      const mostrar = await Projeto.findAll(); // Busca todos os registros
+      res.json(mostrar); // Retorna os registros em formato JSON
+  }catch(error) {
+      res.status(500).json({message:`Erro ao buscar alunos: ${error}`});
+  }
+  
+});
+
+// Projeto.create({
+//   nome: "Jonh Jones",
+//   descricao: "Projeto de arquitetura urbana rudimentar",
+//   data:"24-Oct-2024",
+//   funcionarios:"Darlan, Bruno, Julia, Kauê e Mago"
+// });
+
+
+app.get("/salvar/:nome/:descricao/:data/:funcionarios", (req, res) => {
+  Projeto.create({
+    nome: req.params.nome,
+    descricao: req.params.descricao,
+    data: req.params.data,
+    funcionarios: req.params.funcionarios,
+  })
+
+  
+});
+
+
+app.delete("deletar/:id", async function (req, res) {
+  const {id} = req.params; 
+  const idNumber = parseInt(id,10);
+
+  const deleted = await Projeto.destroy({
+    where: {id: idNumber},
+  });
+
+  if(deleted) {
+    res.json({mensagem: "Evento deletado com sucesso"});
+  }else {
+    res.status(404).json({mensagem: "Evento não encontrado"});
+  }
+  
+});
+
+
+
+//SEMPRE MANTENHA NO FINAL DO CÒDIGO 
+app.listen(3031, function () {
+  console.log("Server is running on port 3031");
+});
